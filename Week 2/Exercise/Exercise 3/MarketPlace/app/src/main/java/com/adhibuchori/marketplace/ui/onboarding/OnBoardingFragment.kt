9d36d3aefa891e9e3101f5dd679415e86e242019
Mochammad Adhi Buchori <adhi.buchori@gmail.com>
2024-06-24 00:19:46 +0700

    Commit Some Adjustment of MarketPlace App

--- Week 2/Exercise/Exercise 3/MarketPlace/app/src/main/java/com/adhibuchori/marketplace/ui/onboarding/OnBoardingFragment.kt
+++ Week 2/Exercise/Exercise 3/MarketPlace/app/src/main/java/com/adhibuchori/marketplace/ui/onboarding/OnBoardingFragment.kt
@@ -9,8 +9,10 @@ import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.plusAssign
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
+import androidx.viewpager2.widget.ViewPager2
 import com.adhibuchori.marketplace.R
 import com.adhibuchori.marketplace.databinding.FragmentOnBoardingBinding
 
@@ -46,28 +48,36 @@ class OnBoardingFragment : Fragment() {
 
         setUpNavigation()
         setUpIndicators()
+        setCurrentIndicator(0)
+
+        binding.vpSliderOnboarding.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
 
         binding.vpSliderOnboarding.adapter = introSlides
     }
 
     private fun setUpIndicators() {
-//        val indicators = arrayOfNulls<ImageView>(introSlides.itemCount)
-//        val layoutParams: LinearLayout.LayoutParams =
-//            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-//        layoutParams.setMargins(16, 0, 16, 0)
-//        for (i in indicators.indices) {
-//            indicators[i] = ImageView(applicationContext)
-//            indicators[i].apply {
-//                this?.setImageDrawable(
-//                    ContextCompat.getDrawable(
-//                        applicationContext,
-//                        R.drawable.indicator_inactive
-//                    )
-//                )
-//                this?.layoutParams = layoutParams
-//            }
-//            binding.indicatorContainer(indicators[i])
-//        }
+        val indicators = arrayOfNulls<ImageView>(introSlides.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(16, 0, 16, 0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(requireContext())
+            indicators[i].apply {
+                this?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.indicator_inactive
+                    )
+                )
+                this?.layoutParams = layoutParams
+            }
+            binding.vIndicatorsContainer.addView(indicators[i])
+        }
     }
 
     private fun setUpNavigation() {
@@ -76,14 +86,59 @@ class OnBoardingFragment : Fragment() {
             .setPopUpTo(R.id.nav_graphs, true)
             .build()
 
-        binding.btnJoinNow.setOnClickListener {
-            Navigation.findNavController(it).navigate(
-                R.id.action_onBoardingFragment_to_loginFragment,
-                null,
-                navOptions
-            )
+        with(binding) {
+            btnJoinNow.setOnClickListener {
+                Navigation.findNavController(it).navigate(
+                    R.id.action_onBoardingFragment_to_loginFragment,
+                    null,
+                    navOptions
+                )
+            }
+
+            tvSkip.setOnClickListener {
+                Navigation.findNavController(it).navigate(
+                    R.id.action_onBoardingFragment_to_loginFragment,
+                    null,
+                    navOptions
+                )
+            }
+
+            tvNext.setOnClickListener {
+                if (vpSliderOnboarding.currentItem + 1 < introSlides.itemCount) {
+                    vpSliderOnboarding.currentItem += 1
+                } else {
+                    Navigation.findNavController(it).navigate(
+                        R.id.action_onBoardingFragment_to_loginFragment,
+                        null,
+                        navOptions
+                    )
+                }
+            }
         }
     }
+
+    private fun setCurrentIndicator(index: Int) {
+        val childCount = binding.vIndicatorsContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView = binding.vIndicatorsContainer.getChildAt(i) as ImageView
+            if (i == index) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.indicator_active
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.indicator_inactive
+                    )
+                )
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
